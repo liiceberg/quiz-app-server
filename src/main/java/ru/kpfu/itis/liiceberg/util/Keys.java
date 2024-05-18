3d2dbd415fa87660de962c0e89d11d221889ffac
liiceberg <2aysylu2003@gmail.com>
2024-05-18 21:37:36 +0300

    delete unused files

--- src/main/java/ru/kpfu/itis/liiceberg/util/Keys.java
+++
@@ -1,7 +0,0 @@
-package ru.kpfu.itis.liiceberg.util;
-
-public class Keys {
-    public static final String RESPONSE_CODE = "response_code";
-    public static final String RESULTS = "results";
-
-}
